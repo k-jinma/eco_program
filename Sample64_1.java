6e47c185c40a2cb5010ac9f78d0c5038871f3e8f
kazuuud <jinmakazuhiro@outlook.jp>
2025-06-28 17:57:01 +0900

    設定ファイルに非Javaリソースを表示するオプションを追加し、不要なJavaファイルとクラスファイルを削除しました。また、CSVファイルを追加し、ファイルからデータを読み込むサンプルプログラムを実装しました。

---
+++ Sample64_1.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
+class Sample64_1{
+	public static void main(String[] args) throws Exception{
+		
+		File file = new File("employees.csv");
+		Scanner scanner = new Scanner(file);
+		
+		String s = scanner.next();
+		System.out.println(s);
+		
+		s = scanner.nextLine();
+		System.out.println(s);
+		
+		scanner.close();
+	}
+}
